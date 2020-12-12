0d83b35d56b67ab05593b507d9c64f6d9cf2a5cb
Flavissimo <Flavissimo@yandex.ru>
2020-12-12 05:28:17 +0300

    Завершила страницу Index

---
+++ source/js/menu.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+  $('.site-list__item').click(function() {
+    $('.main-navigation__toggle').addClass('collapsed').attr('aria-expanded', 'false');
+    $('.main-navigation').removeClass('collapse').removeClass('show').addClass('collapse');
+  });
+
+  var $navBar = $('.sticky-top');
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 200) {
+      $navBar.addClass('border-b');
+    } else {
+      $navBar.removeClass('border-b');
+    }
+  });
+});
